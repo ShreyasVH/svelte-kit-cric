31591f15fc3198e820c9cbaf8d8965977e7b35e6
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-28 12:37:58 +0530

    partial changes

--- src/routes/tours/detail/+page.js
+++ src/routes/tours/detail/+page.js
@@ -1,5 +1,6 @@
 export function load({ url }) {
-    const id = parseInt(url.searchParams.get('id'));
+    // const id = parseInt(url.searchParams.get('id'));
+    const id = url.searchParams.get('id');
     return {
         id
     };
