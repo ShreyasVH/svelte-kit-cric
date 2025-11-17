d75b37b89d599803e9341e2255441f5630da4eb3
ShreyasVH <shreyas.deve.1@gmail.com>
2025-11-17 18:45:48 +0530

    fab for filters

---
+++ src/routes/players/stats/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+import Fab, { Icon } from '@smui/fab';
+
+</script>
+
+<div>
+    Stats
+
+    <Fab color="primary" style="position:absolute; bottom: 16px; right: 16px">
+        <Icon class="material-icons">filter_list</Icon>
+    </Fab>
+</div>
